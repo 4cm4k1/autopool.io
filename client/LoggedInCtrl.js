4f1c12bca35c54f2806167d3ae3f953ea2198e33
Anthony Maki <4cm4k1@gmail.com>
2016-08-04 10:31:38 -0500

    UI change on loggedin, remove unnecessary dep inject in controller

--- client/LoggedInCtrl.js
+++ client/LoggedInCtrl.js
@@ -2,14 +2,17 @@
     'use strict';
     angular.module('autopoolIoApp').controller('LoggedInCtrl', LoggedInCtrl);
 
-    LoggedInCtrl.$inject = ['$http', 'LoginService', 'currentAuth'];
+    LoggedInCtrl.$inject = ['$http', 'currentAuth'];
 
-    function LoggedInCtrl($http, LoginService, currentAuth) {
+    function LoggedInCtrl($http, currentAuth) {
         var vm = this;
 
         vm.displayName = currentAuth.displayName;
+        vm.photoURL = currentAuth.photoURL;
+        vm.email = currentAuth.email;
 
-        console.log('Signed in as:', vm.displayName);
+        console.log('Signed in as:', vm.displayName, vm.email);
+        console.log('Full user object:', currentAuth);
         // currentAuth.$onAuthStateChanged(function(firebaseUser) {
         //     vm.firebaseUser = firebaseUser;
         //     console.log('firebaseUser:', vm.firebaseUser);
