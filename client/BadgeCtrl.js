d4ce1330d83bf5dc0ceff6ee9005746ef479ed1f
Anthony Maki <4cm4k1@gmail.com>
2016-08-08 14:26:12 -0500

    Refactor user info from onboard view to live exclusively in badge VC

--- client/BadgeCtrl.js
+++ client/BadgeCtrl.js
@@ -1,2 +1,20 @@
 //  This will control the badge that appears in the top-right corner when
 //  a user is in an authenticated session
+(function() {
+    'use strict';
+
+    angular.module('autopoolIoApp').controller('BadgeCtrl', BadgeCtrl);
+
+    BadgeCtrl.$inject = ['Auth'];
+
+    function BadgeCtrl(Auth) {
+        var vm = this;
+
+        vm.auth = Auth;
+
+        vm.auth.$onAuthStateChanged(function(firebaseUser) {
+            vm.user = firebaseUser;
+            console.log('BadgeCtrl:', vm.user);
+        });
+    }
+})();
