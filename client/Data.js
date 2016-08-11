c711462ed4c53da01fd1db727da0679fc4a29b42
Anthony Maki <4cm4k1@gmail.com>
2016-08-11 17:03:47 -0500

    Connect Firebase db to HomeCtrl and Data, add user to /users when user signs in

--- client/Data.js
+++ client/Data.js
@@ -1,1 +1,57 @@
 //  This will be the data factory service
+(function() {
+    'use strict';
+
+    angular.module('autopoolIoApp').factory('Data', ['$firebaseObject', '$firebaseArray', Data]);
+
+    function Data($firebaseObject, $firebaseArray) {
+        var data = {};
+
+        //  Save new user
+        data.saveNewUser = function(firebaseUid, displayName, googleEmail, photoURL, googleUid) {
+            var ref = firebase.database().ref();
+            var userRef = ref.child('users').child(firebaseUid);
+
+            var user = $firebaseObject(userRef);
+
+            user.displayName = displayName;
+            user.googleEmail = googleEmail;
+            user.photoURL = photoURL;
+            user.googleUid = googleUid;
+            user.startPoint = null;
+            user.endPoint = null;
+            user.contactEmail = null;
+            user.phoneNumber = null;
+            user.phoneType = null;
+            user.role = {
+                type: null,
+                distances: null
+            };
+            user.latLong = null;
+            user.createDate = firebase.database.ServerValue.TIMESTAMP;
+            user.modDate = firebase.database.ServerValue.TIMESTAMP;
+            user.isAdmin = false;
+            user.isAuth = false;
+
+            user.$save().then(function() {
+                console.log('New user saved:', user.displayName, user.$id);
+            }).catch(function(error) {
+                console.error('Error:', error);
+            });
+
+            return user;
+        };
+
+        //  Get a users info by uid and make it available as a synchronized obj
+        data.getAndUpdateUserInfo = function(firebaseUid) {
+            var ref = firebase.database().ref();
+
+            var user = $firebaseObject(ref.child('users').child(firebaseUid));
+
+            return user;
+        };
+
+        //  Make data methods publicly available to rest of app
+        return data;
+    }
+})();
--- client/HomeCtrl.js
+++ client/HomeCtrl.js
@@ -2,9 +2,9 @@
     'use strict';
     angular.module('autopoolIoApp').controller('HomeCtrl', HomeCtrl);
 
-    HomeCtrl.$inject = ['$http', 'Auth', '$location'];
+    HomeCtrl.$inject = ['$http', 'Auth', '$location', 'Data'];
 
-    function HomeCtrl($http, Auth, $location) {
+    function HomeCtrl($http, Auth, $location, Data) {
         var vm = this;
 
         Auth.$onAuthStateChanged(function(firebaseUser){
@@ -20,6 +20,11 @@
             Auth.$signInWithPopup("google").then(function(firebaseUser) {
                 console.log("Signed in as:", firebaseUser.user.displayName, firebaseUser.user.email);
                 console.log('Info:', firebaseUser);
+
+                Data.saveNewUser(firebaseUser.user.uid, firebaseUser.user.displayName, firebaseUser.user.email, firebaseUser.user.photoURL, firebaseUser.user.providerData[0].uid);
+
+                console.log('Checked DB and found:', Data.getAndUpdateUserInfo(firebaseUser.user.uid));
+
                 $location.path('/onboard');
             }).catch(function(error) {
                 console.log("Authentication failed:", error);
